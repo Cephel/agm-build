d634fc56baed350a6cdbd3439af5980974bc8533
Cephei <cepheidev+github@gmail.com>
2014-05-26 22:28:30 +0200

    Some fixes
    Fixed some issues with quotation marks
    Added pause at the end

--- build.bat
+++ build.bat
@@ -1,14 +1,17 @@
 :: SETUP ::
 :: Put the location of your AddonBuilder.exe here ::
-SET builder="D:\Games\Steam\SteamApps\common\Arma 3 Tools\AddonBuilder.exe"
+SET builder=D:\Games\Steam\SteamApps\common\Arma 3 Tools\AddonBuilder.exe
 
 :: Put the source of the Project here ( where the AGM_ folders are ) ::
-SET source="D:\Documents\Projects\AGM"
+SET source=D:\Documents\Projects\AGM
 
 :: Put the place where you want the pbos to land here ::
-SET destination="D:\Documents\Projects\AGM\@AGM"
+SET destination=D:\Documents\Projects\agm-build\@AGM
 
 :: DON'T TOUCH ME ::
+CD %source%
 IF NOT EXIST %destination% MKDIR %destination%
 IF NOT EXIST %destination%\addons MKDIR %destination%\addons
-FOR /D %%G in ("AGM_*") DO %builder% "%source%\%%G" "%destination%\addons" -PACK -CLEAR
+FOR /D %%G in ("AGM_*") DO "%builder%" "%source%\%%G" "%destination%\addons" -PACK -CLEAR
+
+PAUSE
