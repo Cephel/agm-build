5072c5db2f4ce1306752277b41895dbf0480739c
Cephei <cepheidev+github@gmail.com>
2014-06-22 22:59:12 +0200

    Made the whole script more general purpose
    Can now be used with pretty much all addons that would benefit from such
    a script

--- build.bat
+++ build.bat
@@ -4,14 +4,17 @@
 :: Put the location of your AddonBuilder.exe here ::
 set builder=D:\Games\Steam\SteamApps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe
 
-:: Put the source of the Project here ( where the AGM_ folders are ) ::
+:: Put the source of the Project here ( where the addon folders are ) ::
 set source=D:\Documents\Projects\AGM
 
 :: Put the place where you want the pbos to land here ::
-set destination=D:\Documents\Projects\agm-build\build
+set destination=%source%\build
+
+:: Put the addon prefix here ( your addons should all start with this, ie. AGM for all the agm addons [ AGM_Medical, AGM_Core, etc. ])
+set prefix=AGM
 
 :: DON'T TOUCH ME ::
 cd %source%
 if not exist "%destination%" mkdir "%destination%"
-for /d %%G in ("AGM_*") do "%builder%" "%source%\%%G" "%destination%" -project="%source%" -packonly -clear
+for /d %%G in ("%prefix%*") do "%builder%" "%source%\%%G" "%destination%" -project="%prefix%" -packonly -clear
 pause
