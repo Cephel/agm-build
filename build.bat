11e7777a84c167a35005922c824abc111e6b1158
Cephei <cepheidev+github@gmail.com>
2014-06-22 22:48:04 +0200

    Disabled binarization again, because it ruins everything

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 :: Setup ::
 :: Put the location of your AddonBuilder.exe here ::
-set builder=D:\Games\Steam\SteamApps\common\Arma 3 Tools\AddonBuilder.exe
+set builder=D:\Games\Steam\SteamApps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe
 
 :: Put the source of the Project here ( where the AGM_ folders are ) ::
 set source=D:\Documents\Projects\AGM
@@ -13,5 +13,5 @@ set destination=D:\Documents\Projects\agm-build\build
 :: DON'T TOUCH ME ::
 cd %source%
 if not exist "%destination%" mkdir "%destination%"
-for /d %%G in ("AGM_*") do "%builder%" "%source%\%%G" "%destination%" -project="%source%" -clear
+for /d %%G in ("AGM_*") do "%builder%" "%source%\%%G" "%destination%" -project="%source%" -packonly -clear
 pause
